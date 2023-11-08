e0c04a4f4c81ec899adf5ad7945e6d8197202728
PardoJuan <149524843+PardoJuan@users.noreply.github.com>
2023-11-08 19:43:19 -0300

    Update prestamo.py
    Actualize prestamo.

--- prestamo.py
+++ prestamo.py
@@ -8,11 +8,19 @@ class Prestamo:
         self.fecha_prestamo = fecha_prestamo
         self.fecha_devolucion = fecha_devolucion
 
-    def calcular_demora(self):
+    def demoro(self):
         # Método para calcular si el libro fue devuelto con demora
         fecha_actual = datetime.now()
-        return (fecha_actual - self.fecha_devolucion).days
+        if fecha_actual >  self.fecha_devolucion:
+            return True
+        else:
+            return False
 
     def fue_devuelto_en_fecha(self):
         # Método para verificar si el libro fue devuelto en fecha
-        return self.calcular_demora() <= 0
+        # Devuelve True si es devuelto en fecha, devuelve false si no.
+        fecha_actual = datetime.now()
+        if fecha_actual < self.fecha_devolucion:
+            return True
+        else:
+            return False
