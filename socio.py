aa8dd31ae4a5b6ca781d474c462e11f3fa0c4fa7
PardoJuan <149524843+PardoJuan@users.noreply.github.com>
2023-11-08 19:42:40 -0300

    Update socio.py
    Agrege los metodos tiene demora() y tiene_maximo_libros_prestados()

--- socio.py
+++ socio.py
@@ -4,15 +4,25 @@ class Socio:
         self.nombre = nombre
         self.libros_prestados = []
 
-    def agregar_libro_prestado(self, libro):
+    def agregar_libro_prestado(self, prestamos):
         # Método para agregar un libro prestado al socio
-        self.libros_prestados.append(libro)
+        if self.tiene_maximo_libros_prestados():
+            self.libros_prestados.append(prestamos)
+        else:
+            print(f"El socio {self.nombre} ya tiene 3 prestamos hechos.")
 
     def tiene_demoras(self):
         # Método para verificar si el socio tiene libros con demora
         # Puedes implementar esta función según tus necesidades
-        pass
+        for libro in self.libros_prestados:
+            if  libro.demoro():
+                print(f"El socio {self.nombre} no devolvio el libro {libro}")
+        
+
 
     def tiene_maximo_libros_prestados(self):
         # Método para verificar si el socio tiene más de tres libros prestados
-        return len(self.libros_prestados) > 3
+        if len(self.libros_prestados) > 3:
+            return True
+        else:
+            return False
