1ec6c454bbc743698cdcb26e8e41524227b5e0c0
alumno305 alumno305 <alumno305@labsis-a3m5.labsis.frc.utn.edu.ar>
2023-11-10 16:05:10 -0300

    c

--- database/database.py
+++ database/database.py
@@ -53,19 +53,7 @@ class BibliotecaDB:
 
         except sqlite3.Error as e:
             messagebox.showerror("Error", f"Error al guardar el socio: {str(e)}")
-    """     
-    def prestar_libro(self, libro):
-        prestado = Prestado()
-        prestado.actualizar_estado(libro)
-
-    def devolver_libro(self, libro, dias_de_retraso):
-        if dias_de_retraso > 30:
-            extraviado = Extraviado()
-            extraviado.actualizar_estado(libro)
-        else:
-            disponible = Disponible()
-            disponible.actualizar_estado(libro)
-"""
+    
     def cerrar(self):
         self.conn.close()
     
